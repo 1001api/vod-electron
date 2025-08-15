620f6fdcd500da5c30282fe19a14e09f6efadd30
Res? <resqiagengr@gmail.com>
2025-08-15 08:52:00 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Router from "svelte-spa-router";
+    import MovieSection from "./lib/MovieSection.svelte";
+    import MovieDetail from "./lib/MovieDetail.svelte";
+
+    const routes = {
+        "/": MovieSection,
+        "/vod/:id": MovieDetail,
+    };
+</script>
+
+<main>
+    <Router {routes} />
+</main>
